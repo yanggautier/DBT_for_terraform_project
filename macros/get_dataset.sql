632218dddaf0d57692c80e9a342c0edbe9eef7a4
yanggautier <yangguole@outlook.com>
2025-08-19 17:06:25 +0200

    add var before modeslls

--- macros/get_dataset.sql
+++
@@ -1,9 +0,0 @@
-{% macro get_dataset(layer) -%}
-    {%- if target.name == 'prod' -%}
-        {{ layer }}_prod
-    {%- elif target.name == 'staging' -%}
-        {{ layer }}_staging
-    {%- else -%}
-        {{ layer }}_dev
-    {%- endif -%}
-{%- endmacro %}
