aae9f75e8a09795f9eec030595cb9657eba6eef1
yanggautier <yangguole@outlook.com>
2025-08-19 19:04:41 +0200

    add environment with macrp get_custom_schema

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -1,13 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-        
-        {{ custom_schema_name | trim}}
-
-    {%- endif -%}
-    
+    {% if custom_schema_name is none %}
+        {{ target.schema }}
+    {% else %}
+        {% if target.name == 'prod' %}
+            {{ custom_schema_name ~ '_prod' }}
+        {% elif target.name == 'staging' %}
+            {{ custom_schema_name ~ '_staging' }}
+        {% else %}
+            {{ custom_schema_name ~ '_dev' }}
+        {% endif %}
+    {% endif %}
 {%- endmacro %}
